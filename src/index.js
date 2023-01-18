253ef4a123f63527eed90897919b7d88c8911e18
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-01-18 16:03:58 +0900

    Init : 기초 초기세팅 완료

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './Router';
+import './styles/common.scss';
+import './styles/reset.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Router />
+);
+
