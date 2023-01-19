6f2d3a9fa3280330fe1e1b13de34105a7185d54e
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-01-19 21:37:01 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

---
+++ src/pages/Suhyeon/Login/components/Container.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useState } from 'react';
+import './Container.scss';
+import { useNavigate } from 'react-router-dom';
+
+function Container() {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/suhyeon-main');
+  };
+
+  const [inputId, setInputId] = useState('');
+  const [inputPw, setInputPw] = useState('');
+  const [btnState, setBtnState] = useState(true);
+
+  const saveUserId = event => {
+    const state = event.target.value;
+    setInputId(state);
+  };
+
+  const saveUserPw = event => {
+    const state = event.target.value;
+    setInputPw(state);
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+        className="userId account"
+        onChange={saveUserId}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        className="userPw account"
+        onChange={saveUserPw}
+      />
+      <button id="btn" onClick={goToMain} disabled={btnState}>
+        로그인
+      </button>
+    </div>
+  );
+}
+
+export default Container;
