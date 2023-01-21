32f2e3c7ae702a2691bdd087dfd915a3232b10d7
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-01-21 13:05:06 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Hein/Login/HeinLogin.js
+++ src/pages/Hein/Login/HeinLogin.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../../Hein/Login/HeinLogin.scss';
 
-function Login() {
+const Login = a => {
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/Main');
   };
+  const [inputId, setInputId] = useState('초기값');
+  const [inputPw, setInputPw] = useState('');
+
+  function saveUserId(e) {
+    setInputId(e.target.value);
+    console.log(e.target.value);
+  }
+  function saveUserPw(e) {
+    setInputPw(e.target.value);
+    console.log(e.target.value);
+  }
 
   return (
     <div className="container">
@@ -14,16 +25,30 @@ function Login() {
         <a href="#none">Westagram</a>
       </div>
       <input
+        onChange={e => {
+          saveUserId(e);
+        }}
         className="id"
         type="text"
-        placeholder=" 전화번호, 사용자 이름 또는 이메일"
+        value={inputId}
+        placeholder="전화번호, 사용자 이름 또는 이메일"
       />
-      <input className="pw" type="password" placeholder=" 비밀번호" />
-      <button onClick={goToMain}>로그인</button>
+      <input
+        onChange={e => {
+          saveUserPw(e);
+        }}
+        className="pw"
+        type="password"
+        value={inputPw}
+        placeholder=" 비밀번호"
+      />
+      <button onClick={goToMain} disabled>
+        로그인
+      </button>
       <div className="forgot">
         <Link to="#">비밀번호를 잊으셨나요?</Link>
       </div>
     </div>
   );
-}
+};
 export default Login;
